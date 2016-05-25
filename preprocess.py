eabf0c89ef17696b36256430e51cac3c6a88600b
Tian Wang <twang95@corn29.stanford.edu>
2016-05-24 21:58:43 -0700

    add data processing

--- preprocess.py
+++ preprocess.py
@@ -14,23 +14,3 @@ targets = ["toy_dataset/cnn_processed/questions/training",
 metadata_directory = "toy_dataset/cnn_processed/metadata"
 
 data_processor.do_all(sources, targets, metadata_directory, 4)
-
-directory = targets[0]
-
-f = open(os.path.join(metadata_directory, 'metadata.txt'), 'r')
-input_length = int(f.readline().split(':')[1])
-query_length = int(f.readline().split(':')[1])
-vocab_size = int(f.readline().split(':')[1])
-f.close()
-
-for i, fn in enumerate(os.listdir(directory)):
-    array = np.load(os.path.join(directory, fn))
-    for sample in array[0]:
-        assert(len(sample) == input_length)
-    for sample in array[1]:
-        assert(len(sample) == query_length)
-    for sample in array[2]:
-        print sample
-
-array = np.load(os.path.join(metadata_directory, "weights.npy"))
-assert(array.shape == (vocab_size + 2, 100))
