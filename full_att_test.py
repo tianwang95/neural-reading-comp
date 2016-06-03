9bcc33bcd669904d6c655000b3a8977f8f297dc4
Kerem Goksel <bkgoksel@gmail.com>
2016-06-03 08:37:30 +0000

    add saving weights per epoch

--- full_att_test.py
+++ full_att_test.py
@@ -1,13 +1,13 @@
- #!/usr/bin/env python 
-
 import os
 from models import attentive_model
 from models.data_generator import DataGenerator
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 import theano
+
 theano.config.floatX = 'float32'
 theano.config.optimization = 'fast_run'
 
-nb_epoch = 7
+nb_epoch = 8
 batch_size = 32
 
 dataset = 'datasets/full_dataset/cnn_processed'
@@ -20,12 +20,12 @@ train_generator = DataGenerator(batch_size, dataset, 'training')
 validation_generator = DataGenerator(batch_size, dataset, 'validation')
 
 print "Calling fit generator"
-
 model.fit_generator(train_generator,
-                                   samples_per_epoch=train_generator.nb_samples_epoch,
-                                   nb_epoch=nb_epoch,
-                                   verbose=2,
-                                   validation_data=validation_generator,
-                                   nb_val_samples=validation_generator.nb_samples_epoch)
+       samples_per_epoch=train_generator.nb_samples_epoch,
+       nb_epoch=nb_epoch,
+       verbose=2,
+       callbacks=[ModelCheckpoint('results/att_model/att_model.{epoch:02d}-{val_loss:.2f}.hdf5')],
+       validation_data=validation_generator,
+       nb_val_samples=validation_generator.nb_samples_epoch)
 
 print "Training done"
--- toy_train_test.py
+++ toy_train_test.py
@@ -1,11 +1,13 @@
 import os
 from models import attentive_model
 from models.data_generator import DataGenerator
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 import theano
+
 theano.config.floatX = 'float32'
 theano.config.optimization = 'fast_run'
 
-nb_epoch = 60
+nb_epoch = 3
 batch_size = 32
 
 dataset = 'datasets/toy_dataset/cnn_processed'
@@ -18,17 +20,12 @@ train_generator = DataGenerator(batch_size, dataset, 'training')
 validation_generator = DataGenerator(batch_size, dataset, 'validation')
 
 print "Calling fit generator"
-
 model.fit_generator(train_generator,
-                                   samples_per_epoch=train_generator.nb_samples_epoch,
-                                   nb_epoch=nb_epoch,
-                                   verbose=2,
-                                   validation_data=validation_generator,
-                                   nb_val_samples=validation_generator.nb_samples_epoch)
+       samples_per_epoch=train_generator.nb_samples_epoch,
+       nb_epoch=nb_epoch,
+       verbose=2,
+       callbacks=[ModelCheckpoint('results/att_model/toy.{epoch:02d}-{val_loss:.2f}.hdf5')],
+       validation_data=validation_generator,
+       nb_val_samples=validation_generator.nb_samples_epoch)
 
 print "Training done"
-
-print "Testing starting"
-
-test_generator = DataGenerator(batch_size, dataset, 'test')
-print model.evaluate_generator(test_generator, 10)
